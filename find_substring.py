21051232a838b8e6208611bab91122fcd9ab49f8
PointerFLY <linghao.michael.ma@gmail.com>
2018-01-22 12:34:30 +0000

    Add function prototype and requirements

--- find_substring.py
+++ find_substring.py
@@ -1,2 +1,9 @@
-def find_substring(string, sub):
+# Writing a function that can find the position of first substring of a string
+# find_substring('hello, trinity', 'tri') == 6
 
+def find_substring(string, sub):
+        """
+        :type string: str
+        :type sub: str
+        :rtype: int
+        """
